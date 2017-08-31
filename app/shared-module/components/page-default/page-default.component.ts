5f94c27ae52cedca0e2dcdca34c1008926fd81f3
Yoseph Santos <yoseph.santos@bematech.com.br>
2017-08-31 17:26:08 -0300

    componentes dynamics

---
+++ app/shared-module/components/page-default/page-default.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'page-default',
+    templateUrl: 'page-default.component.html' 
+})
+
+export class PageDefaultComponent {}
